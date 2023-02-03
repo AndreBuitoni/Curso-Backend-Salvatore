c2c8e705d579ec78387eab26b75181753fb48d11
André Buitoni <a.buitoni@gmail.com>
2023-02-03 10:15:07 -0300

    endpoint /oi criado

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
+// Endpoint / -> Hello World
 app.get("/", function (req, res) {
   res.send("Este é meu site, gostou né?");
+});
+
+// Endpoint /oi -> Olá Mundo
+app.get("/oi", function (req, res) {
+  res.send("Olá, Mundo!");
 });
 
 app.listen(3000);
