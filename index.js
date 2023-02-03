a24ee1812cfe40f7b579781ffea9e7cd1fa34cce
André Buitoni <a.buitoni@gmail.com>
2023-02-03 11:21:32 -0300

    Endpoint tipo read all criado

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const app = express();
 
 // Endpoint / -> Hello World
 app.get("/", function (req, res) {
-  res.send("Este é meu site, gostou né?");
+  res.send("Hello, World!");
 });
 
 // Endpoint /oi -> Olá Mundo
 app.get("/oi", function (req, res) {
   res.send("Olá, Mundo!");
+});
+
+// Lista de Informações
+const itens = ["Margarita", "Cosmopolitan", "Tequila Sunrise"];
+
+// CRUD -> Lista de Informações
+
+// Endpoint Read All -> [GET] /item
+app.get("/item", function (req, res) {
+  res.send(itens);
 });
 
 app.listen(3000);
