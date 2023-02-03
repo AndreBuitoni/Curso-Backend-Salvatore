cf3ee746a0a9875f156f6c4062c4b1e8df4f0334
André Buitoni <a.buitoni@gmail.com>
2023-02-03 09:51:23 -0300

    Nodemon instalado e configurado

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Este é meu site");
+  res.send("Este é meu site, gostou né?");
 });
 
 app.listen(3000);
