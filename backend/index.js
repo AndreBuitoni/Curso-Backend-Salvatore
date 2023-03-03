374a7a9bb79dbee01f8171b80164f449e490c016
André Buitoni <a.buitoni@gmail.com>
2023-03-03 12:42:34 -0300

    Updated Project with latest modifications

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
+const cors = require("cors");
 
 // localhost ou 127.0.0.1
 // const DB_URL = "mongodb://127.0.0.1:27017";
@@ -16,6 +17,9 @@ async function main() {
 
   const app = express();
 
+  // Habilita o CORS
+  app.use(cors());
+
   // O que vier no body da requisição, está em JSON
   app.use(express.json());
 
@@ -29,8 +33,9 @@ async function main() {
     res.send("Olá, mundo!");
   });
 
-  // Lista de Informações
-  const itens = ["Margarita", "Cosmopolitan", "Tequila Sunrise"];
+  // Lista de informações
+  const itens = ["Rick Sanchez", "Morty Smith", "Summer Smith"];
+  //              0               1              2
 
   // CRUD -> Lista de informações
 
@@ -51,37 +56,40 @@ async function main() {
   app.post("/item", async function (req, res) {
     // console.log(req.body);
     const item = req.body;
-    await collection.insertOne(item)
+    await collection.insertOne(item);
     res.send(item);
   });
 
-  // Endpoint UPDATE -> [PUT] /item/:item
+  // Endpoint Update -> [PUT] /item/:id
   app.put("/item/:id", async function (req, res) {
     const id = req.params.id;
     const body = req.body;
 
     // console.log(id, body);
-    await collection.updateOne (
-      { _id: new ObjectId(id) },
-      { $set: body }
-    );
+
+    await collection.updateOne({ _id: new ObjectId(id) }, { $set: body });
 
     res.send(body);
   });
 
-  // Endpoint DELETE -> [DELETE] /item/:id
+  // Endpoint Delete -> [DELETE] /item/:id
   // Exercício:
-  // pesquisar sobre a operação de remover itens
-  // implementar o endpoint de delete
-  // realizar a operação de excluir item
+  // - pesquisar sobre a operação de remover itens
+  // - implementar o endpoint de delete
+  // - realizar a operação de excluir item
 
-  app.delete("/item/:id", async function(req, res) {
-    const id = req.params.id
-    await collection.deleteOne({ _id: new ObjectId(id) })
-    res.send("Registro removido com sucesso!")
-  })
+  app.delete("/item/:id", async function (req, res) {
+    const id = req.params.id;
 
-  app.listen(3000);
+    await collection.deleteOne({ _id: new ObjectId(id) });
+
+    res.send("Registro removido com sucesso!");
+  });
+
+  const port = process.env.PORT || 3000;
+  app.listen(port, function () {
+    console.log("Servidor rodando na porta: " + port);
+  });
 }
 
 main();
--- frontend/src/components/ReadAll/ReadAll.jsx
+++ frontend/src/components/ReadAll/ReadAll.jsx
@@ -1,35 +1,37 @@
-import "./ReadAll.css"
-import Card from "../Card/Card"
+import "./ReadAll.css";
+import Card from "../Card/Card";
+import { useEffect, useState } from "react";
 
 // Mock Items (exatamente a mesma estrutura que o back traria)
 // Isso facilita o trabalho aqui no Front, para conseguir estruturar
 // comportamento, sem precisar depender do back para receber dados
-const items = [
-    {
-        _id: "63ee1e0b18f2b9a93da8435a",
-        nome: "Rick Sanchez",
-        imagemUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-    },
-    {
-        _id: "63ef87c74522de2944ab1fa8",
-        nome: "Morty Smith",
-        imagemUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-    },
-    {
-        _id: "63ef87cc4522de2944ab1fa9",
-        nome: "Summer Smith",
-        imagemUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
-    },
-    {
-        _id: "63ef87d44522de2944ab1faa",
-        nome: "Beth Smith",
-        imagemUrl: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
-    },
-    {
-        _id: "63ef87e24522de2944ab1fab",
-        nome: "Jerry Smith",
-        imagemUrl: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
-    },
+const itemsMock = [
+  {
+    _id: "63ee1e0b18f2b9a93da8435a",
+    nome: "Rick Sanchez",
+    imagemUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    tags: ["Status: Vivo", "Espécie: Humana", "Origem: Terra C-137"],
+  },
+  {
+    _id: "63ef87c74522de2944ab1fa8",
+    nome: "Morty Smith",
+    imagemUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+  },
+  {
+    _id: "63ef87cc4522de2944ab1fa9",
+    nome: "Summer Smith",
+    imagemUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+  },
+  {
+    _id: "63ef87d44522de2944ab1faa",
+    nome: "Beth Smith",
+    imagemUrl: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+  },
+  {
+    _id: "63ef87e24522de2944ab1fab",
+    nome: "Jerry Smith",
+    imagemUrl: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+  },
 ];
 
 /*
@@ -47,13 +49,42 @@ const items = [
 // inserido dentro delas
 
 function ReadAll() {
-    return <div className="ReadAll">
-        {items.map(function (item) {
-            // console.log(item);
-            // Key -> card-1234
-            return <Card key={'card-' + item._id} item={item} />;
-        })}
+  // useState retorna 2 coisas:
+  // 1: o valor do estado
+  // 2: a função que atualiza o valor do estado
+  //   const estadoItems = useState([]);
+  //   const items = estadoItems[0];
+  //   const setItems = estadoItems[1];
+  const [items, setItems] = useState([]);
+
+  // Realizar requisição para backend obtendo a lista de itens
+  async function realizarRequisicao() {
+    // const url = "http://localhost:3000/item";
+    const url = "https://ocean-jornada-fullstack-fevereiro-23-jmkv.onrender.com/item";
+    const response = await fetch(url);
+    const data = await response.json();
+
+    setItems(data);
+  }
+
+  // UseEffect
+  // 1: uma função que será executada
+  // 2: uma lista de dependências
+  useEffect(function () {
+    realizarRequisicao();
+  }, []);
+
+  //   console.log(67, items);
+
+  return (
+    <div className="ReadAll">
+      {items.map(function (item) {
+        // console.log(item);
+        // Key -> card-1234
+        return <Card key={"card-" + item._id} item={item} />;
+      })}
     </div>
+  );
 }
 
 export default ReadAll;
